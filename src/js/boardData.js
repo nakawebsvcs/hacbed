0cf10b0ef65b65a37ee04f91b641ee549177fee3
nakawebsvcs <info@nakamurawebservices.com>
2025-03-12 10:01:27 -1000

    Our Team and Our Board setup and configuration

---
+++ src/js/boardData.js
@@ -0,0 +1,56 @@
+/**
+ * BOARD MEMBERS DATA FILE
+ *
+ * HOW TO EDIT:
+ * 1. To add a new team member: Copy an existing entry and paste it at the end of the array
+ * 2. To remove a team member: Delete their entire entry (everything between and including the curly braces {})
+ * 3. To update information: Simply edit the text between the quotes for any property
+ * 4. To update photos: Upload new photos to the /public/assets/images/ folder and update the imageUrl path
+ *
+ * NOTE: Don't forget to add/remove commas as needed between entries!
+ */
+
+export const boardMembers = [
+  {
+    name: "Brent Kakesako",
+    jobTitle: "Executive Director",
+    bio: "Brent feels humbled to co-learn alongside communities across Hawai'i and beyond and blessed with an awesome team he supports as best he can. He is driven by his family upbringing, the connection he feels to this place, and his initial eye opening community engagement experiences. Brent also dabbled in supporting the startup ecosystem, founded a small consulting business, coaches wrestling, and wipes out a lot at Kewalo's. He received his J.D. from the William S. Richardson School of Law, an M.B.A. from the Shidler College of Business, and his Bachelors from Harvard University focused on the study of teams.",
+    imageUrl: "/assets/images/Brent-Kakesako.jpg",
+  },
+  {
+    name: "Merri Keli'ikuli",
+    jobTitle: "Office Manager & Program Support",
+    bio: "Merri Keli'ikuli is responsible for day to day management of the office, accounting/bookkeeping, and support across various programs/projects. Merri's work experience includes her previous role as Executive Director of Hoa 'Āina O Mākaha, overseeing daily operations, grant and budget management, and community outreach. Prior to that, she led several teams in retail operations while executing merchant planning and buying for more than 25 years. Merri received her bachelor's degree in Business Administration from Loyola Marymount University.",
+    imageUrl: "/assets/images/Merri-Keliikuli.jpg",
+  },
+  {
+    name: "Corin Thornburg",
+    jobTitle: "Senior Community-Based Economic Development Project Associate",
+    bio: "Corin Thornburg supports the team with current activities as HACBED grows partnerships and collaborative projects. Prior to HACBED, Corin worked as a teacher and grant writer at Hakipu'u Learning Center, Public Charter School in Kāne'ohe, O'ahu. Following graduation, Corin worked at the University of San Diego, supporting local procurement initiatives in Linda Vista, California. She is grateful to all who have guided, mentored, and taught her. Corin grew up in 'Ewa Beach and Mānana on O'ahu. She received her B.A. in Psychology from Azusa Pacific University and M.A .from the University of San Diego in Social Innovation.",
+    imageUrl: "/assets/images/Corin-Thornburg.jpeg",
+  },
+  {
+    name: "Merri Keli'ikuli",
+    jobTitle: "Office Manager & Program Support",
+    bio: "Merri Keli'ikuli is responsible for day to day management of the office, accounting/bookkeeping, and support across various programs/projects. Merri's work experience includes her previous role as Executive Director of Hoa 'Āina O Mākaha, overseeing daily operations, grant and budget management, and community outreach. Prior to that, she led several teams in retail operations while executing merchant planning and buying for more than 25 years. Merri received her bachelor's degree in Business Administration from Loyola Marymount University.",
+    imageUrl: "/assets/images/Merri-Keliikuli.jpg",
+  },
+  {
+    name: "Merri Keli'ikuli",
+    jobTitle: "Office Manager & Program Support",
+    bio: "Merri Keli'ikuli is responsible for day to day management of the office, accounting/bookkeeping, and support across various programs/projects. Merri's work experience includes her previous role as Executive Director of Hoa 'Āina O Mākaha, overseeing daily operations, grant and budget management, and community outreach. Prior to that, she led several teams in retail operations while executing merchant planning and buying for more than 25 years. Merri received her bachelor's degree in Business Administration from Loyola Marymount University.",
+    imageUrl: "/assets/images/Merri-Keliikuli.jpg",
+  },
+  {
+    name: "Merri Keli'ikuli",
+    jobTitle: "Office Manager & Program Support",
+    bio: "Merri Keli'ikuli is responsible for day to day management of the office, accounting/bookkeeping, and support across various programs/projects. Merri's work experience includes her previous role as Executive Director of Hoa 'Āina O Mākaha, overseeing daily operations, grant and budget management, and community outreach. Prior to that, she led several teams in retail operations while executing merchant planning and buying for more than 25 years. Merri received her bachelor's degree in Business Administration from Loyola Marymount University.",
+    imageUrl: "/assets/images/Merri-Keliikuli.jpg",
+  },
+  {
+    name: "Merri Keli'ikuli",
+    jobTitle: "Office Manager & Program Support",
+    bio: "Merri Keli'ikuli is responsible for day to day management of the office, accounting/bookkeeping, and support across various programs/projects. Merri's work experience includes her previous role as Executive Director of Hoa 'Āina O Mākaha, overseeing daily operations, grant and budget management, and community outreach. Prior to that, she led several teams in retail operations while executing merchant planning and buying for more than 25 years. Merri received her bachelor's degree in Business Administration from Loyola Marymount University.",
+    imageUrl: "/assets/images/Merri-Keliikuli.jpg",
+  },
+];
